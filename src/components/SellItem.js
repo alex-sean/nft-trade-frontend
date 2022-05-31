58f1f53fc8e9c1d5d5dee113a736c5af2f9c48fa
Marco-John535 <62136420+Marco-John535@users.noreply.github.com>
2022-05-31 10:18:37 +0800

    code cleaning

--- src/components/SellItem.js
+++ src/components/SellItem.js
@@ -1,18 +1,15 @@
 import React from 'react';
-import useScrollTrigger from '@mui/material/useScrollTrigger';
-import PropTypes from 'prop-types';
-import { Grid, Typography, Box } from '@mui/material';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { Typography, Box } from '@mui/material';
 
 function SellItem(props) {
-  const { src, title, subtitle, amount, like } = props;
+  const { src, title, subtitle } = props;
   
   return (
     <Box sx={{padding: '16px', textAlign: 'center'}}>
       <img src={src} />
       <Typography variant="h6" color="#131740">{title}</Typography>
       <Typography color="#5A5D79">{subtitle}</Typography>
-     </Box>
+    </Box>
   );
 }
 
