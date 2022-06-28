fb50abdf5fb099e38b2353229cad912953ca1e25
AlexRos101 <ros1996101@gmail.com>
2022-06-29 04:13:05 +0800

    update: Update card item

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -3,14 +3,13 @@ import { Typography, Box, Link } from '@mui/material';
 import HistoryIcon from '@mui/icons-material/History';
 import useStyles from '../styles/styles';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 function CardItem(props) {
   const { token } = props;
   const classes = useStyles();
   
   return (
-    <Box className={`${classes.hoverShadow} ${classes.paperBackground}`} sx={{border:'solid 1px rgba(0, 0, 0, 0.1)', borderRadius:'1.25rem', padding: '1.1875rem', marginBottom: '16px'}}>
+    <Box className={`${classes.hoverShadow} ${classes.paperBackground}`} sx={{border:'solid 1px rgba(0, 0, 0, 0.1)', borderRadius:'1.25rem', padding: '1.1875rem', marginBottom: '16px'}} onClick={() => document.location.href=`/item/${token.collectionAddress}/${token.tokenID}`}>
       <Box sx={{position: 'relative'}}>
         <img style={{width: '100%'}} src={`${token.imageURL}`} className={classes.rounded}/>
         <Box sx={{display:'flex', position: 'absolute', top: '16px', right: '16px', padding: '8px', background: '#fff', borderRadius: '8px'}} className={classes.hotBidLike}>
@@ -31,7 +30,7 @@ function CardItem(props) {
         {/* <MoreHorizIcon /> */}
         {`$ ${token.price} USD`}
       </Box>
-      <Typography className={classes.collectionSubtitle}>{token.description.slice(0, 30)}...</Typography>
+      <Typography className={[classes.collectionSubtitle, classes.ellipseText]}>{token.description}</Typography>
       <Box sx={{display:'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: '16px'}}>
         <Typography className={classes.collectionBuy}>Buy now</Typography>
         <Box sx={{display:'flex', color: '#5A5D79'}}>
