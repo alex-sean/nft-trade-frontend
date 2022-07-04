323ccc9ddeb3251f47103a95a0a74fcaefc303f0
Marco-John535 <62136420+Marco-John535@users.noreply.github.com>
2022-07-04 11:40:51 +0800

    Tooltip

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Typography, Box, Link } from '@mui/material';
+import { Typography, Box, Link, Tooltip } from '@mui/material';
 import HistoryIcon from '@mui/icons-material/History';
 import useStyles from '../styles/styles';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
@@ -91,10 +91,14 @@ function CardItem(props) {
         </Box>
         <Box sx={{display:'flex', position: 'absolute', bottom: '-12px', left: '16px'}}>
           <Link>
-            <img src="../images/avatars/creator_1.png" className={classes.creatorLink}/>
+            <Tooltip title="Creator: Sussygirl">
+              <img src="../images/avatars/creator_1.png" className={classes.creatorLink}/>
+            </Tooltip>
           </Link>
           <Link sx={{marginLeft: '-6px'}}>
-            <img src="../images/avatars/owner_1.png" className={classes.creatorLink}/>
+            <Tooltip title="Owner: Sussygirl">
+              <img src="../images/avatars/owner_1.png" className={classes.creatorLink}/>
+            </Tooltip>
           </Link>
         </Box>
       </Box>
